3e69fc552cd6e18bee3fe1601a608a15fb097738
kkalvro <alvrocerejadesouza@gmail.com>
2026-03-21 14:59:06 -0300

    xd

--- PI 3 Sistema Autônomo 2.0/PI 3 Sistema Autônomo 2.0/Form1.Designer.cs
+++ PI 3 Sistema Autônomo 2.0/PI 3 Sistema Autônomo 2.0/Form1.Designer.cs
@@ -43,6 +43,17 @@
             this.lblNome = new System.Windows.Forms.Label();
             this.lblSenha = new System.Windows.Forms.Label();
             this.lblGrupo = new System.Windows.Forms.Label();
+            this.btnIniciarPartida = new System.Windows.Forms.Button();
+            this.lblIdJogador = new System.Windows.Forms.Label();
+            this.lblSenhaJogador = new System.Windows.Forms.Label();
+            this.lblNomeJogador = new System.Windows.Forms.Label();
+            this.txtNomeJogador = new System.Windows.Forms.TextBox();
+            this.lblDigitaSenhaJogador = new System.Windows.Forms.Label();
+            this.txtSenhaJogador = new System.Windows.Forms.TextBox();
+            this.txtEntrarID = new System.Windows.Forms.Label();
+            this.txtEntrarNaSala = new System.Windows.Forms.TextBox();
+            this.btnEntrarNaPartida = new System.Windows.Forms.Button();
+            this.lstMaoJogador = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnConsulta
@@ -58,7 +69,7 @@
             // lbl4
             // 
             this.lbl4.AutoSize = true;
-            this.lbl4.Location = new System.Drawing.Point(715, 344);
+            this.lbl4.Location = new System.Drawing.Point(738, 31);
             this.lbl4.Name = "lbl4";
             this.lbl4.Size = new System.Drawing.Size(40, 13);
             this.lbl4.TabIndex = 1;
@@ -67,16 +78,17 @@
             // lstListadePartidas
             // 
             this.lstListadePartidas.FormattingEnabled = true;
-            this.lstListadePartidas.Location = new System.Drawing.Point(219, 121);
+            this.lstListadePartidas.Location = new System.Drawing.Point(214, 47);
             this.lstListadePartidas.Name = "lstListadePartidas";
             this.lstListadePartidas.Size = new System.Drawing.Size(218, 173);
             this.lstListadePartidas.TabIndex = 2;
+            this.lstListadePartidas.SelectedIndexChanged += new System.EventHandler(this.lstDadosPartida_SelectedIndexChanged_1);
             // 
             // btnCriarPartida
             // 
-            this.btnCriarPartida.Location = new System.Drawing.Point(76, 290);
+            this.btnCriarPartida.Location = new System.Drawing.Point(12, 193);
             this.btnCriarPartida.Name = "btnCriarPartida";
-            this.btnCriarPartida.Size = new System.Drawing.Size(100, 57);
+            this.btnCriarPartida.Size = new System.Drawing.Size(87, 57);
             this.btnCriarPartida.TabIndex = 4;
             this.btnCriarPartida.Text = "Criar";
             this.btnCriarPartida.UseVisualStyleBackColor = true;
@@ -85,7 +97,7 @@
             // lstDadosPartida
             // 
             this.lstDadosPartida.FormattingEnabled = true;
-            this.lstDadosPartida.Location = new System.Drawing.Point(567, 119);
+            this.lstDadosPartida.Location = new System.Drawing.Point(573, 47);
             this.lstDadosPartida.Name = "lstDadosPartida";
             this.lstDadosPartida.Size = new System.Drawing.Size(205, 173);
             this.lstDadosPartida.TabIndex = 8;
@@ -93,21 +105,21 @@
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(76, 118);
+            this.txtNome.Location = new System.Drawing.Point(73, 46);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(100, 20);
             this.txtNome.TabIndex = 9;
             // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(76, 176);
+            this.txtSenha.Location = new System.Drawing.Point(73, 97);
             this.txtSenha.Name = "txtSenha";
             this.txtSenha.Size = new System.Drawing.Size(100, 20);
             this.txtSenha.TabIndex = 10;
             // 
             // txtGrupo
             // 
-            this.txtGrupo.Location = new System.Drawing.Point(76, 234);
+            this.txtGrupo.Location = new System.Drawing.Point(73, 145);
             this.txtGrupo.Name = "txtGrupo";
             this.txtGrupo.Size = new System.Drawing.Size(100, 20);
             this.txtGrupo.TabIndex = 11;
@@ -115,7 +127,7 @@
             // lblNomeDaPartida
             // 
             this.lblNomeDaPartida.AutoSize = true;
-            this.lblNomeDaPartida.Location = new System.Drawing.Point(460, 162);
+            this.lblNomeDaPartida.Location = new System.Drawing.Point(455, 145);
             this.lblNomeDaPartida.Name = "lblNomeDaPartida";
             this.lblNomeDaPartida.Size = new System.Drawing.Size(86, 13);
             this.lblNomeDaPartida.TabIndex = 6;
@@ -124,7 +136,7 @@
             // lblID
             // 
             this.lblID.AutoSize = true;
-            this.lblID.Location = new System.Drawing.Point(460, 121);
+            this.lblID.Location = new System.Drawing.Point(460, 120);
             this.lblID.Name = "lblID";
             this.lblID.Size = new System.Drawing.Size(69, 13);
             this.lblID.TabIndex = 5;
@@ -133,7 +145,7 @@
             // lblDataDaPartida
             // 
             this.lblDataDaPartida.AutoSize = true;
-            this.lblDataDaPartida.Location = new System.Drawing.Point(460, 207);
+            this.lblDataDaPartida.Location = new System.Drawing.Point(460, 173);
             this.lblDataDaPartida.Name = "lblDataDaPartida";
             this.lblDataDaPartida.Size = new System.Drawing.Size(81, 13);
             this.lblDataDaPartida.TabIndex = 7;
@@ -151,7 +163,7 @@
             // lblNome
             // 
             this.lblNome.AutoSize = true;
-            this.lblNome.Location = new System.Drawing.Point(25, 121);
+            this.lblNome.Location = new System.Drawing.Point(22, 49);
             this.lblNome.Name = "lblNome";
             this.lblNome.Size = new System.Drawing.Size(38, 13);
             this.lblNome.TabIndex = 13;
@@ -160,7 +172,7 @@
             // lblSenha
             // 
             this.lblSenha.AutoSize = true;
-            this.lblSenha.Location = new System.Drawing.Point(25, 179);
+            this.lblSenha.Location = new System.Drawing.Point(22, 100);
             this.lblSenha.Name = "lblSenha";
             this.lblSenha.Size = new System.Drawing.Size(41, 13);
             this.lblSenha.TabIndex = 14;
@@ -169,17 +181,123 @@
             // lblGrupo
             // 
             this.lblGrupo.AutoSize = true;
-            this.lblGrupo.Location = new System.Drawing.Point(30, 238);
+            this.lblGrupo.Location = new System.Drawing.Point(22, 148);
             this.lblGrupo.Name = "lblGrupo";
-            this.lblGrupo.Size = new System.Drawing.Size(39, 13);
+            this.lblGrupo.Size = new System.Drawing.Size(21, 13);
             this.lblGrupo.TabIndex = 15;
-            this.lblGrupo.Text = "Grupo:";
+            this.lblGrupo.Text = "ID:";
+            // 
+            // btnIniciarPartida
+            // 
+            this.btnIniciarPartida.Location = new System.Drawing.Point(114, 193);
+            this.btnIniciarPartida.Name = "btnIniciarPartida";
+            this.btnIniciarPartida.Size = new System.Drawing.Size(80, 57);
+            this.btnIniciarPartida.TabIndex = 16;
+            this.btnIniciarPartida.Text = "Iniciar Partida";
+            this.btnIniciarPartida.UseVisualStyleBackColor = true;
+            this.btnIniciarPartida.Click += new System.EventHandler(this.btnIniciarPartida_Click);
+            // 
+            // lblIdJogador
+            // 
+            this.lblIdJogador.AutoSize = true;
+            this.lblIdJogador.Location = new System.Drawing.Point(236, 262);
+            this.lblIdJogador.Name = "lblIdJogador";
+            this.lblIdJogador.Size = new System.Drawing.Size(74, 13);
+            this.lblIdJogador.TabIndex = 17;
+            this.lblIdJogador.Text = "ID do Jogador";
+            // 
+            // lblSenhaJogador
+            // 
+            this.lblSenhaJogador.AutoSize = true;
+            this.lblSenhaJogador.Location = new System.Drawing.Point(236, 290);
+            this.lblSenhaJogador.Name = "lblSenhaJogador";
+            this.lblSenhaJogador.Size = new System.Drawing.Size(88, 13);
+            this.lblSenhaJogador.TabIndex = 18;
+            this.lblSenhaJogador.Text = "Senha da partida";
+            // 
+            // lblNomeJogador
+            // 
+            this.lblNomeJogador.AutoSize = true;
+            this.lblNomeJogador.Location = new System.Drawing.Point(13, 286);
+            this.lblNomeJogador.Name = "lblNomeJogador";
+            this.lblNomeJogador.Size = new System.Drawing.Size(94, 13);
+            this.lblNomeJogador.TabIndex = 20;
+            this.lblNomeJogador.Text = "Nome do Jogador:";
+            // 
+            // txtNomeJogador
+            // 
+            this.txtNomeJogador.Location = new System.Drawing.Point(113, 283);
+            this.txtNomeJogador.Name = "txtNomeJogador";
+            this.txtNomeJogador.Size = new System.Drawing.Size(100, 20);
+            this.txtNomeJogador.TabIndex = 19;
+            // 
+            // lblDigitaSenhaJogador
+            // 
+            this.lblDigitaSenhaJogador.AutoSize = true;
+            this.lblDigitaSenhaJogador.Location = new System.Drawing.Point(14, 319);
+            this.lblDigitaSenhaJogador.Name = "lblDigitaSenhaJogador";
+            this.lblDigitaSenhaJogador.Size = new System.Drawing.Size(91, 13);
+            this.lblDigitaSenhaJogador.TabIndex = 22;
+            this.lblDigitaSenhaJogador.Text = "Senha da partida:";
+            // 
+            // txtSenhaJogador
+            // 
+            this.txtSenhaJogador.Location = new System.Drawing.Point(114, 316);
+            this.txtSenhaJogador.Name = "txtSenhaJogador";
+            this.txtSenhaJogador.Size = new System.Drawing.Size(100, 20);
+            this.txtSenhaJogador.TabIndex = 21;
+            // 
+            // txtEntrarID
+            // 
+            this.txtEntrarID.AutoSize = true;
+            this.txtEntrarID.Location = new System.Drawing.Point(14, 354);
+            this.txtEntrarID.Name = "txtEntrarID";
+            this.txtEntrarID.Size = new System.Drawing.Size(60, 13);
+            this.txtEntrarID.TabIndex = 24;
+            this.txtEntrarID.Text = "ID da Sala:";
+            // 
+            // txtEntrarNaSala
+            // 
+            this.txtEntrarNaSala.Location = new System.Drawing.Point(114, 351);
+            this.txtEntrarNaSala.Name = "txtEntrarNaSala";
+            this.txtEntrarNaSala.Size = new System.Drawing.Size(100, 20);
+            this.txtEntrarNaSala.TabIndex = 23;
+            // 
+            // btnEntrarNaPartida
+            // 
+            this.btnEntrarNaPartida.Location = new System.Drawing.Point(16, 381);
+            this.btnEntrarNaPartida.Name = "btnEntrarNaPartida";
+            this.btnEntrarNaPartida.Size = new System.Drawing.Size(198, 57);
+            this.btnEntrarNaPartida.TabIndex = 25;
+            this.btnEntrarNaPartida.Text = "Entrar na partida";
+            this.btnEntrarNaPartida.UseVisualStyleBackColor = true;
+            this.btnEntrarNaPartida.Click += new System.EventHandler(this.btnEntrarNaPartida_Click);
+            // 
+            // lstMaoJogador
+            // 
+            this.lstMaoJogador.FormattingEnabled = true;
+            this.lstMaoJogador.Location = new System.Drawing.Point(239, 319);
+            this.lstMaoJogador.Name = "lstMaoJogador";
+            this.lstMaoJogador.Size = new System.Drawing.Size(158, 121);
+            this.lstMaoJogador.TabIndex = 26;
+            this.lstMaoJogador.SelectedIndexChanged += new System.EventHandler(this.lstMaoJogador_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstMaoJogador);
+            this.Controls.Add(this.btnEntrarNaPartida);
+            this.Controls.Add(this.txtEntrarID);
+            this.Controls.Add(this.txtEntrarNaSala);
+            this.Controls.Add(this.lblDigitaSenhaJogador);
+            this.Controls.Add(this.txtSenhaJogador);
+            this.Controls.Add(this.lblNomeJogador);
+            this.Controls.Add(this.txtNomeJogador);
+            this.Controls.Add(this.lblSenhaJogador);
+            this.Controls.Add(this.lblIdJogador);
+            this.Controls.Add(this.btnIniciarPartida);
             this.Controls.Add(this.lblGrupo);
             this.Controls.Add(this.lblSenha);
             this.Controls.Add(this.lblNome);
@@ -219,6 +337,17 @@
         private System.Windows.Forms.Label lblNome;
         private System.Windows.Forms.Label lblSenha;
         private System.Windows.Forms.Label lblGrupo;
+        private System.Windows.Forms.Button btnIniciarPartida;
+        private System.Windows.Forms.Label lblIdJogador;
+        private System.Windows.Forms.Label lblSenhaJogador;
+        private System.Windows.Forms.Label lblNomeJogador;
+        private System.Windows.Forms.TextBox txtNomeJogador;
+        private System.Windows.Forms.Label lblDigitaSenhaJogador;
+        private System.Windows.Forms.TextBox txtSenhaJogador;
+        private System.Windows.Forms.Label txtEntrarID;
+        private System.Windows.Forms.TextBox txtEntrarNaSala;
+        private System.Windows.Forms.Button btnEntrarNaPartida;
+        private System.Windows.Forms.ListBox lstMaoJogador;
     }
 }
 
--- PI 3 Sistema Autônomo 2.0/PI 3 Sistema Autônomo 2.0/Form1.cs
+++ PI 3 Sistema Autônomo 2.0/PI 3 Sistema Autônomo 2.0/Form1.cs
@@ -1,23 +1,21 @@
 ﻿using Draft;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PI_3_Sistema_Autônomo_2._0
 {
     public partial class Form1 : Form
     {
+        string idJogadorSalvo = "";
+        string senhaJogadorSalvo = "";
+
         public Form1()
         {
             InitializeComponent();
             lbl4.Text = Jogo.versao;
+
+            txtGrupo.ReadOnly = true;
+            txtGrupo.TabStop = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -25,7 +23,6 @@ namespace PI_3_Sistema_Autônomo_2._0
             string retorno = Jogo.ListarPartidas("T");
 
             retorno = retorno.Replace("\r", "");
-
             string[] partidas = retorno.Split('\n');
 
             lstListadePartidas.Items.Clear();
@@ -33,14 +30,15 @@ namespace PI_3_Sistema_Autônomo_2._0
             foreach (string p in partidas)
             {
                 if (p.Trim() != "")
-                {
                     lstListadePartidas.Items.Add(p);
-                }
             }
         }
 
         private void lstDadosPartida_SelectedIndexChanged_1(object sender, EventArgs e)
         {
+            if (lstListadePartidas.SelectedItem == null)
+                return;
+
             string partida = lstListadePartidas.SelectedItem.ToString();
             string[] dadosPartida = partida.Split(',');
 
@@ -52,12 +50,15 @@ namespace PI_3_Sistema_Autônomo_2._0
             lblNomeDaPartida.Text = nomePartida;
             lblDataDaPartida.Text = data;
 
+            txtGrupo.Text = idPartida.ToString();
+
             string retorno = Jogo.ListarJogadores(idPartida);
 
             retorno = retorno.Replace("\r", "");
             string[] jogadores = retorno.Split('\n');
 
             lstDadosPartida.Items.Clear();
+
             for (int i = 0; i < jogadores.Length - 1; i++)
             {
                 lstDadosPartida.Items.Add(jogadores[i]);
@@ -70,56 +71,83 @@ namespace PI_3_Sistema_Autônomo_2._0
             string senha = txtSenha.Text;
             string grupo = txtGrupo.Text;
 
-            if (nome == "" || senha == "" || grupo == "")
+            if (nome == "" || senha == "")
             {
-                MessageBox.Show("Erro: Todos os campos são obrigatórios!",
-                "Meu PI 3", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Erro: Nome e senha são obrigatórios!");
                 return;
             }
 
-            foreach (string item in lstListadePartidas.Items)
-            {
-                if (item.Contains("," + nome + ","))
-                {
-                    MessageBox.Show("Erro: Já existe uma partida com esse nome!",
-                    "Meu PI 3", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-
-                if (item.Contains("," + senha + ","))
-                {
-                    MessageBox.Show("Erro: Já existe uma partida com essa senha!",
-                    "Meu PI 3", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    return;
-                }
-            }
+            if (grupo == "")
+                grupo = "0";
 
             string retorno = Jogo.CriarPartida(nome, senha, grupo);
 
+
+
             if (retorno.Substring(0, 4) == "ERRO")
             {
-                MessageBox.Show("Erro:\n" + retorno.Substring(5),
-                "Meu PI 3", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Erro:\n" + retorno.Substring(5));
                 return;
             }
 
-            MessageBox.Show("Partida criada com sucesso!",
-            "Meu PI 3", MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-
-            string retornoLista = Jogo.ListarPartidas("T");
-
-            retornoLista = retornoLista.Replace("\r", "");
-            retornoLista = retornoLista.Substring(0, retornoLista.Length - 1);
-            string[] partidas = retornoLista.Split('\n');
-
-            lstListadePartidas.Items.Clear();
-
+            MessageBox.Show("Partida criada com sucesso!");
             button1_Click(null, null);
 
             txtNome.Clear();
             txtSenha.Clear();
             txtGrupo.Clear();
         }
+
+
+        private void btnEntrarNaPartida_Click(object sender, EventArgs e)
+        {
+            string nomeJogador = txtNomeJogador.Text.Trim();
+            string senhaJogador = txtSenhaJogador.Text.Trim();
+            string idTexto = txtEntrarNaSala.Text.Trim();
+
+            if (nomeJogador == "" || senhaJogador == "" || idTexto == "")
+            {
+                MessageBox.Show("Preencha Nome, Senha e ID da Partida!");
+                return;
+            }
+
+            int idPartida;
+
+            if (!int.TryParse(idTexto, out idPartida))
+            {
+                MessageBox.Show("ID inválido!");
+                return;
+            }
+
+            string retorno = Jogo.Entrar(idPartida, nomeJogador, senhaJogador);
+
+            if (retorno.StartsWith("ERRO"))
+            {
+                MessageBox.Show("Erro:\n" + retorno.Substring(5));
+                return;
+            }
+
+            string[] partes = retorno.Split(',');
+
+            if (partes.Length < 2)
+            {
+                MessageBox.Show("Formato inesperado:\n" + retorno);
+                return;
+            }
+
+            string idJogador = partes[0];
+
+            lblIdJogador.Text = idJogador;
+            lblSenhaJogador.Text = senhaJogador;
+
+            MessageBox.Show("Entrou na partida!");
+        }
+
+        // eventos vazios (pra não dar erro)
+       
+        private void btnIniciarPartida_Click(object sender, EventArgs e) { }
+        private void lstMaoJogador_SelectedIndexChanged(object sender, EventArgs e) { }
+
+      
     }
 }
